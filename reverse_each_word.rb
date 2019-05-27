91b4e6fe6ea9da01aad40b045c69890498dcda92
Amanda King <amapickell@gmail.com>
2019-05-27 16:21:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
 def  reverse_each_word (string)
   array = string.split(/ /)
-  array.collect! {|word| word.reverse}
-  return array.join(" ")
+  reverse_array = []
+  array.collect! {|word| reverse_array << word.reverse}
+  return reverse_array.join(" ")
 end
